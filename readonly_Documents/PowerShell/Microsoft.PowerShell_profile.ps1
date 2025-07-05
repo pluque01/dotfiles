b22c3dc0236133c5e37bff79a58486750927931c
Pablo Luque Salguero <pablols114@gmail.com>
2025-07-05 12:39:46 +0200

    feat(pwsh): include eza to replace ls

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,15 @@
 # ENV variables
 $Env:KOMOREBI_CONFIG_HOME = 'C:\Users\pablo\.config\komorebi'
 
+# Zoxide (better cd) configuration
 Set-Alias cd z -Option AllScope
+
+# Eza (better ls) configuration
+del alias:ls
+function ls {
+    eza -al --icons=always --color=always --group-directories-first
+}
+
 Invoke-Expression (&starship init powershell)
 Import-Module Terminal-Icons
 Set-PSReadLineOption -PredictionViewStyle ListView
